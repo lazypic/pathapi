29d0fabb5020c9fb1fa79ebbdae1d2a1d4011fff
woong <hello@lazypic.org>
2019-01-28 12:15:55 +0900

    add pathapi, testcode

--- pathapi.py
+++ pathapi.py
@@ -2,34 +2,57 @@
 #coding:utf8
 import re
 
-
 def project(path):
 	"""
-	경로를 넣으면 project 이름을 반환한다.
+	경로에서 project 이름을 반환한다.
 	"""
-	p = re.findall('/project/(\S[^/]+)', path.replace("\\","/"))
+	p = re.findall('/project/(\w+)', path.replace("\\","/"))
 	if len(p) != 1:
 		return "", "경로에서 project 정보를 가지고 올 수 없습니다."
 	return p[0], None
 
 def seq(path):
 	"""
-	경로를 넣으면 seq 이름을 반환한다.
+	경로에서 seq 이름을 반환한다.
 	"""
-	p = re.findall('/shot/(\S[^/]+)', path.replace("\\","/"))
+	p = re.findall('/shot/(\w+)', path.replace("\\","/"))
 	if len(p) != 1:
 		return "", "경로에서 seq 정보를 가지고 올 수 없습니다."
 	return p[0], None
 
-if __name__ == "__main__":
-	path = "/project/circle/shot/FOO/0010/comp/FOO_0010_comp_v001.nk"
-	projectName, err = project(path)
-	if err:
-		print err
-	print projectName
+def shot(path):
+	"""
+	경로에서 shot 이름을 반환한다.
+	"""
+	p = re.findall('/shot/\w+/(\w+)', path.replace("\\","/"))
+	if len(p) != 1:
+		return "", "경로에서 shot 정보를 가지고 올 수 없습니다."
+	return p[0], None
 
-	s, err = seq(path)
-	if err:
-		print err
-	print s
+def task(path):
+	"""
+	경로에서 task 이름을 반환한다.
+	"""
+	p = re.findall('/shot/\w+/\w+/(\w+)', path.replace("\\","/"))
+	if len(p) != 1:
+		return "", "경로에서 task 정보를 가지고 올 수 없습니다."
+	return p[0], None
+
+def ver(path):
+	"""
+	경로에서 version 정보를 반환한다.
+	"""
+	p = re.findall('_v(\d+)', path.replace("\\","/"))
+	if len(p) != 1:
+		return -1, "경로에서 task 정보를 가지고 올 수 없습니다."
+	return int(p[0]), None
+
+def seqnum(path):
+	"""
+	경로에서 시퀀스 넘버를 반환한다.
+	"""
+	p = re.findall('\.(\d+)\.', path.replace("\\","/"))
+	if len(p) != 1:
+		return -1, "경로에서 seqnum 정보를 가지고 올 수 없습니다."
+	return int(p[0]), None
 
